9f8031699d3dfd2639156a592dc04db2bb531e6d
Seb Kay <seb@sebkay.com>
2026-01-21 19:04:52 +0000

    Add login functionality

--- resources/views/pages/auth/⚡login.blade.php
+++ resources/views/pages/auth/⚡login.blade.php
@@ -1,10 +1,48 @@
 <?php
 
+use Illuminate\Validation\ValidationException;
 use Livewire\Attributes\Layout;
+use Livewire\Attributes\Validate;
 use Livewire\Component;
 
 new #[Layout('layouts::guest')] class extends Component {
-    //
+    #[Validate(['required', 'email', 'exists:users'])]
+    public string $email = '';
+
+    #[Validate(['required'])]
+    public string $password = '';
+
+    #[Validate(['nullable'])]
+    public bool $remember = false;
+
+    #[Validate(['nullable', 'string'])]
+    public string $redirect = '';
+
+    public function mount()
+    {
+        $this->email = config('seed.users.super.email');
+        $this->password = config('seed.users.super.password');
+        $this->remember = true;
+        $this->redirect = request()->query('redirect', '');
+    }
+
+    public function login()
+    {
+        $this->validate();
+
+        throw_if(
+            !auth()->guard()->attempt($this->only('email', 'password'), $this->remember),
+            ValidationException::withMessages([
+                'email' => __('auth.failed'),
+            ]),
+        );
+
+        if ($this->redirect) {
+            return redirect($this->redirect);
+        }
+
+        return redirect()->intended(route('home'));
+    }
 };
 ?>
 
@@ -12,14 +50,18 @@ new #[Layout('layouts::guest')] class extends Component {
     <livewire:page-title text="Log In" />
 
     <div class="bg-white rounded-2xl xl:p-10 p-6 border border-brand-200">
-        <form>
+        <form wire:submit="login">
             <div class="form-row">
                 <div class="form-col">
                     <label class="label" for="email">
                         Email
                     </label>
-                    <input id="email" class="input" type="email" name="email" required />
-                    {{-- <FieldError :message="errors.email" /> --}}
+
+                    <input id="email" class="input" type="email" name="email" wire:model="email" required />
+
+                    @error('email')
+                        <x-field-error>{{ $message }}</x-field-error>
+                    @enderror
                 </div>
 
                 <div class="form-col">
@@ -27,22 +69,32 @@ new #[Layout('layouts::guest')] class extends Component {
                         Password
                         <a href="#" class="text-link">Forgot password?</a>
                     </label>
-                    <input id="password" class="input" type="password" name="password" required />
-                    {{-- <FieldError :message="errors.password" /> --}}
+
+                    <input id="password" class="input" type="password" name="password" wire:model="password"
+                        required />
+
+                    @error('password')
+                        <x-field-error>{{ $message }}</x-field-error>
+                    @enderror
                 </div>
 
                 <div class="form-col">
                     <label class="toggle">
-                        <input class="sr-only peer" type="checkbox" name="remember" value="1" />
+                        <input class="sr-only peer" type="checkbox" name="remember" value="1"
+                            wire:model="remember" />
                         <div>
                         </div>
                         <span>
                             Remember me
                         </span>
                     </label>
+
+                    @error('remember')
+                        <x-field-error>{{ $message }}</x-field-error>
+                    @enderror
                 </div>
 
-                <input type="hidden" name="redirect" />
+                <input type="hidden" name="redirect" wire:model="redirect" />
 
                 <div class="form-col">
                     <button class="button button-full">
