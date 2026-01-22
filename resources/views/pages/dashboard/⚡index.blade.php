c140dc48fbc9ffb45559f95b454968cd693993b3
Seb Kay <seb@sebkay.com>
2026-01-22 20:58:54 +0000

    Add content to dashboard

--- resources/views/pages/dashboard/⚡index.blade.php
+++ resources/views/pages/dashboard/⚡index.blade.php
@@ -1,12 +1,31 @@
 <?php
 
+use App\Models\User;
 use Livewire\Component;
 
-new class extends Component {
-    //
+new class extends Component
+{
+    public string $title = 'Dashboard';
+
+    public User $user;
+
+    public function mount()
+    {
+        $this->user = auth()->guard()->user();
+    }
 };
 ?>
 
 <div>
-    dashboard
+    <h1 class="xl:text-4xl text-3xl font-medium text-neutral-900 xl:mb-8 mb-4">
+        {{ $title }}
+    </h1>
+
+    <div class="bg-white rounded-2xl xl:p-10 p-6 border border-brand-200">
+        @if ($user->can(\App\Enums\Permission::CREATE_POSTS))
+            <p>User can create posts.</p>
+        @else
+            <p>User can't create posts.</p>
+        @endif
+    </div>
 </div>
